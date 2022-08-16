1dac4b26a448b306bca1bf73a88055e2b66e124a
hongpingliu <liuhongping_2008@163.com>
2022-08-16 14:16:38 +0800

    add controller demo

--- src/main/java/com/gitdemo/demo/controller/TestController.java
+++ src/main/java/com/gitdemo/demo/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.gitdemo.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("demo")
 public class TestController {
+    @GetMapping("/name")
     private String getName(){
         return "name01";
     }
