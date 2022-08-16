4b80e140c5feb3dfa15ae72921b12e1409c02c71
hongpingliu <liuhongping_2008@163.com>
2022-08-16 14:08:13 +0800

    test01

--- src/main/java/com/gitdemo/demo/controller/TestController.java
+++ src/main/java/com/gitdemo/demo/controller/TestController.java
@@ -4,4 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private String getName(){
+        return "name01";
+    }
+    private String getGrade(){
+        return "8"
+    }
 }
